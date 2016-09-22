ef003ff7053fda16b0283c9ec257b89de61a6522
Siddharth Bhola <sbhola@internal.tavisca.com>
2016-09-22 16:22:21 +0530

    play framework for web api in scala

---
+++ my-first-app/app/controllers/TestController.scala
@@ -0,0 +1,45 @@
+package controllers
+
+import javax.inject._
+
+import models._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+
+/**
+  * Created by sbhola on 9/21/2016.
+  */
+@Singleton
+class TestController extends Controller{
+
+  def testMethod = Action {
+    Ok("hello world my first test method.")
+  }
+
+  def savePlace = Action(BodyParsers.parse.json) { request =>
+    val placeResult = request.body.validate[Place]
+    placeResult.fold(
+      errors => {
+        BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors)))
+      },
+      place => {
+        Place.save(place)
+        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+place.name+"' saved.") ))
+      }
+    )
+  }
+
+  implicit val locationReads: Reads[Location] = (
+    (JsPath \ "lat").read[Double] and
+      (JsPath \ "long").read[Double]
+    )(Location.apply _)
+
+  implicit val placeReads: Reads[Place] = (
+    (JsPath \ "name").read[String] and
+      (JsPath \ "location").read[Location]
+    )(Place.apply _)
+
+}
